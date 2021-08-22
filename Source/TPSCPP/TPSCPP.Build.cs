49d2d14d075876f910d822f7e3a172f867c50dfd
minhkhoi0975 <minhkhoi0975@gmail.com>
2021-08-21 22:10:39 -0400

    Added a module NavigationSystem so that the library UNavigationSystemV1 can be used.

--- Source/TPSCPP/TPSCPP.Build.cs
+++ Source/TPSCPP/TPSCPP.Build.cs
@@ -8,6 +8,6 @@ public class TPSCPP : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule", "UMG", "GameplayTasks" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule", "UMG", "GameplayTasks", "NavigationSystem"});
 	}
 }
