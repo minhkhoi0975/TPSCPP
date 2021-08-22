0c7872286eb40d4183423177a288d43f80e7d128
minhkhoi0975 <minhkhoi0975@gmail.com>
2021-08-22 14:17:55 -0400

    Implemeted AI spawner.

--- Source/TPSCPP/Characters/CharacterSpawner.cpp
+++ Source/TPSCPP/Characters/CharacterSpawner.cpp
@@ -2,26 +2,58 @@
 
 
 #include "CharacterSpawner.h"
+#include "CharacterBase.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "UObject/ConstructorHelpers.h"
+#include "TimerManager.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
 
 // Sets default values
-ACharacterSpawner::ACharacterSpawner()
+ACharacterSpawner::ACharacterSpawner(): Super()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// Dummy.
+	Dummy = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Dummy"));
+	Dummy->RelativeRotation = FRotator(0.0f, -90.0f, 0.0f);
+	Dummy->bHiddenInGame = true;
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SkeletalMesh(TEXT("/Game/AnimStarterPack/UE4_Mannequin/Mesh/SK_Mannequin"));
+	if (SkeletalMesh.Succeeded())
+	{
+		Dummy->SetSkeletalMesh(SkeletalMesh.Object);
+	}
 
+	/*
+	// Behavior Tree.
+	static ConstructorHelpers::FObjectFinder<UBehaviorTree> BehaviorTree(TEXT("/Game/MyAssets/AI/AIBehaviorTree"));
+	if (BehaviorTree.Succeeded())
+	{
+		this->BehaviorTree = BehaviorTree.Object;
+	}
+	*/
+
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	// No need to load this on client.
+	bNetLoadOnClient = false;
 }
 
 // Called when the game starts or when spawned
 void ACharacterSpawner::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	SpawnNPC();
+	GetWorld()->GetTimerManager().SetTimer(SpawnTimerHandle, this, &ACharacterSpawner::SpawnNPC, Cooldown, true);
 }
 
-// Called every frame
-void ACharacterSpawner::Tick(float DeltaTime)
+void ACharacterSpawner::SpawnNPC()
 {
-	Super::Tick(DeltaTime);
+	APawn* Pawn = UAIBlueprintHelperLibrary::SpawnAIFromClass(GetWorld(), CharacterClass, BehaviorTree, GetActorLocation(), GetActorRotation(), true);
 
+	ACharacterBase* Character = Cast<ACharacterBase>(Pawn);
+	if (IsValid(Character))
+	{
+		Character->AIPath = AIPath;
+	}
 }
 
--- Source/TPSCPP/Characters/CharacterSpawner.h
+++ Source/TPSCPP/Characters/CharacterSpawner.h
@@ -10,6 +10,31 @@ UCLASS()
 class TPSCPP_API ACharacterSpawner : public AActor
 {
 	GENERATED_BODY()
+
+public:
+	/**Used to visualize the spawner. Hidden in game.*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class USkeletalMeshComponent* Dummy;
+
+public:
+	/**The template of spawned NPCs.*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<class ACharacterBase> CharacterClass;
+
+	/** How long before another character is spawned.*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Cooldown;
+
+	/** If this variable is set, it will override the default behavior tree of the character.*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UBehaviorTree* BehaviorTree;
+
+	/** It this variable is set, the spawned NPC will travel along the path. Otherwise, it will wander.*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class AAIPath* AIPath;
+
+	//UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	FTimerHandle SpawnTimerHandle;
 	
 public:	
 	// Sets default values for this actor's properties
@@ -19,8 +44,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
+public:
+	// UFUNCTION(BlueprintCallable)
+	void SpawnNPC();
 };
